b76e55aa7b235be99f22891218d9cc0d9b646574
Aliyan Aftab <aliyanaftab2009@gmail.com>
2024-12-10 14:41:19 +0500

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/footer";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
+        <Navbar />
         {children}
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,47 @@
-import About from "@/components/About";
-import BlogCards from "@/components/BlogCard";
-import CategorySection from "@/components/CategorySection";
-import Client from "@/components/Client";
-import HeroSection from "@/components/HeroSection";
-import MeetOurChef from "@/components/MetOurChef";
-import OurMenu from "@/components/OurMenu";
-import Testimonials from "@/components/Testimonials";
-import VideoActive from "@/components/VideoActive";
-import WhyChooseSection from "@/components/WhyChooseSection";
+'use client';
 
-export default function Home() {
+import Image from 'next/image';
+import React, { FC } from 'react';
+
+
+// import React from 'react'
+import Hero from './components/Hero'
+import AbountUs from './components/AbountUs'
+import FoodCategory from './components/FoodCategory'
+import Items from './components/Items'
+import MeetShef from './components/MeetShef'
+import MenuItem from './components/MenuItem'
+import RestaurantActive from './components/RestaurantActive'
+import Blog from './components/Blog'
+import Testimonials from './components/Testimonials';
+
+// import ExtraTaste from './components/ExtraTaste'
+
+
+
+
+
+function page() {
   return (
-    <div className="bg-[#0D0D0D] px-[220px] w-full">
-      <div className="space-y-52">
-        <div>
-          <HeroSection />
-        </div>
-        <div>
-          <About />
-        </div>
-        <div>
-          <CategorySection />
-        </div>
-        <div>
-          <WhyChooseSection />
-        </div>
-        <div>
-          <Client />
-        </div>
-        <div>
-          <OurMenu />
-        </div>
-        <MeetOurChef />
-        <Testimonials />
-        <VideoActive />
-        <BlogCards />
-      </div>
-    </div>
-  );
+<>    
+    
+    
+
+    <Hero />
+    <AbountUs />
+    <FoodCategory />
+    {/* <ExtraTaste /> */}
+    <Items />
+
+    <MenuItem />
+    <MeetShef />
+    
+    <Testimonials />
+    <RestaurantActive />
+    <Blog />
+    
+  </>
+  )
 }
+
+export default page
